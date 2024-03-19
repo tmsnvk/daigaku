c3e9cff81df09952506f969600d8a158320422bf
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-03-19 20:43:04 +0100

    refactor: refactor university entity as per base entity super classes

--- backend/src/main/java/net/tamasnovak/entities/university/University.java
+++ backend/src/main/java/net/tamasnovak/entities/university/University.java
@@ -4,43 +4,21 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PastOrPresent;
+import net.tamasnovak.entities.BaseEntity;
 import net.tamasnovak.entities.application.Application;
 import net.tamasnovak.entities.country.Country;
 
-import java.sql.Timestamp;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 
 @Entity
 @Table(name = "universities")
-public final class University {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id", updatable = false, nullable = false)
-  private long id;
-
-  @Column(name = "uuid", updatable = false, nullable = false)
-  @org.hibernate.validator.constraints.UUID
-  private java.util.UUID uuid;
-
-  @Column(name = "created_at", updatable = false, nullable = false)
-  @PastOrPresent
-  private Timestamp createdAt;
-
-  @Column(name = "last_updated_at", nullable = false)
-  @PastOrPresent
-  private Timestamp lastUpdatedAt;
-
+public final class University extends BaseEntity {
   @Column(name = "name", nullable = false)
   @NotBlank
   private String name;
@@ -73,22 +51,6 @@ public final class University {
     this.country = country;
     this.address = address;
     this.applications = new HashSet<>();
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public UUID getUuid() {
-    return uuid;
-  }
-
-  public Timestamp getCreatedAt() {
-    return createdAt;
-  }
-
-  public Timestamp getLastUpdatedAt() {
-    return lastUpdatedAt;
   }
 
   public String getName() {
