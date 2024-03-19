b83d9d6d77a2df947f7a27e6c78e5e926a407b70
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-03-19 18:41:24 +0100

    refactor: refactor application status entity as per base entity super classes

--- backend/src/main/java/net/tamasnovak/entities/application/ApplicationStatus.java
+++ backend/src/main/java/net/tamasnovak/entities/application/ApplicationStatus.java
@@ -1,45 +1,16 @@
 package net.tamasnovak.entities.application;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PastOrPresent;
+import net.tamasnovak.entities.BaseStatusEntity;
 
-import java.sql.Timestamp;
-import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 
 @Entity
 @Table(name = "application_status")
-public final class ApplicationStatus {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id", updatable = false, nullable = false)
-  private long id;
-
-  @Column(name = "uuid", updatable = false, nullable = false)
-  @org.hibernate.validator.constraints.UUID
-  private UUID uuid;
-
-  @Column(name = "created_at", updatable = false, nullable = false)
-  @PastOrPresent
-  private Timestamp createdAt;
-
-  @Column(name = "last_updated_at", nullable = false)
-  @PastOrPresent
-  private Timestamp lastUpdatedAt;
-
-  @Column(name = "name", nullable = false)
-  @NotBlank
-  private String name;
-
+public final class ApplicationStatus extends BaseStatusEntity {
   @OneToMany(mappedBy = "applicationStatusId")
   @JsonManagedReference
   private Set<Application> applications;
@@ -47,28 +18,7 @@ public final class ApplicationStatus {
   public ApplicationStatus() {}
 
   public ApplicationStatus(String name) {
-    this.name = name;
-    this.applications = new HashSet<>();
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public UUID getUuid() {
-    return uuid;
-  }
-
-  public Timestamp getCreatedAt() {
-    return createdAt;
-  }
-
-  public Timestamp getLastUpdatedAt() {
-    return lastUpdatedAt;
-  }
-
-  public String getName() {
-    return name;
+    super(name);
   }
 
   public Set<Application> getApplications() {
