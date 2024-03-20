b7e4f04b393428651ee638ff0851dd28157895ce
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-03-20 16:39:26 +0100

    refactor: remove multiple interfaces from pending account service

--- backend/src/main/java/net/tamasnovak/controllers/pendingAccount/PendingAccountController.java
+++ backend/src/main/java/net/tamasnovak/controllers/pendingAccount/PendingAccountController.java
@@ -3,9 +3,9 @@ package net.tamasnovak.controllers.pendingAccount;
 import jakarta.validation.Valid;
 import net.tamasnovak.dtos.account.access.PendingAccountRegistrationDto;
 import net.tamasnovak.dtos.email.NewEmailDto;
-import net.tamasnovak.services.account.account.AccountService;
+import net.tamasnovak.services.account.account.AccountServiceImpl;
 import net.tamasnovak.services.email.EmailService;
-import net.tamasnovak.services.account.pendingAccount.PendingAccountService;
+import net.tamasnovak.services.account.pendingAccount.PendingAccountServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api/pending-accounts")
 public class PendingAccountController {
-  private final AccountService accountService;
-  private final PendingAccountService pendingAccountService;
+  private final AccountServiceImpl accountServiceImpl;
+  private final PendingAccountServiceImpl pendingAccountServiceImpl;
   private final EmailService emailService;
   private final PendingAccountControllerConstants pendingAccountControllerConstants;
 
   @Autowired
-  public PendingAccountController(AccountService accountService, PendingAccountService pendingAccountService, EmailService emailService, PendingAccountControllerConstants pendingAccountControllerConstants) {
-    this.accountService = accountService;
-    this.pendingAccountService = pendingAccountService;
+  public PendingAccountController(AccountServiceImpl accountServiceImpl, PendingAccountServiceImpl pendingAccountServiceImpl, EmailService emailService, PendingAccountControllerConstants pendingAccountControllerConstants) {
+    this.accountServiceImpl = accountServiceImpl;
+    this.pendingAccountServiceImpl = pendingAccountServiceImpl;
     this.emailService = emailService;
     this.pendingAccountControllerConstants = pendingAccountControllerConstants;
   }
@@ -36,10 +36,10 @@ public class PendingAccountController {
     consumes = "application/json"
   )
   public ResponseEntity<HttpStatus> register(@Valid @RequestBody PendingAccountRegistrationDto registrationData) {
-    pendingAccountService.checkIfExistsByEmail(registrationData.email());
-    accountService.checkIfExistsByEmail(registrationData.email());
+    pendingAccountServiceImpl.checkIfExistsByEmail(registrationData.email());
+    accountServiceImpl.checkIfExistsByEmail(registrationData.email());
 
-    pendingAccountService.addAccount(registrationData);
+    pendingAccountServiceImpl.addAccount(registrationData);
 
     NewEmailDto newEmail = new NewEmailDto(
       registrationData.email(),
