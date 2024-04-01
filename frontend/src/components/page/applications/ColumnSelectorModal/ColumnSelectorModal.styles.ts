7c137323b1a280ad1e3f137e03b54ff3eeba26fe
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-04-01 17:35:05 +0200

    style: style applications table display modal window

--- frontend/src/components/page/applications/ColumnSelectorModal/ColumnSelectorModal.styles.ts
+++ frontend/src/components/page/applications/ColumnSelectorModal/ColumnSelectorModal.styles.ts
@@ -1,8 +1,41 @@
 import styled from 'styled-components';
+import { BaseElementBorderStyle } from '@components/shared/base-styles';
 
 const ModalContainer = styled.dialog`
-  width: 10rem;
-  border: 1rem solid red;
+  ${BaseElementBorderStyle};
+  
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 5rem 7.5rem 5rem 7.5rem;
+  margin: 5% auto 0 auto;
+  
+  & article {
+    display: flex;
+    flex-direction: row;
+    margin: 1.5rem 0 0 0;
+    
+    & input {
+      margin: 0 1.5rem 0 0;
+      
+      &:focus,
+      &:hover {
+        outline: none;
+      }
+    }
+  }
+  
+  & button {
+    margin: 2.5rem auto 0 auto;
+    background-color: transparent;
+    font-size: inherit;
+    font-weight: 800;
+    cursor: pointer;
+
+    &:hover {
+      color: ${({ theme }) => theme.color.tertiaryLight};
+    }
+  }
 `;
 
 export {
