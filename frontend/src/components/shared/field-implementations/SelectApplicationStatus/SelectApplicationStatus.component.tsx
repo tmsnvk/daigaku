d5db76927f74e15b0a60b3799ece045994662fa0
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-04-06 11:14:37 +0200

    feat: implement loading and error handling in SelectApplicationStatus.component.tsx

--- frontend/src/components/shared/field-implementations/SelectApplicationStatus/SelectApplicationStatus.component.tsx
+++ frontend/src/components/shared/field-implementations/SelectApplicationStatus/SelectApplicationStatus.component.tsx
@@ -8,6 +8,7 @@ import {
   ErrorMessage,
   InputFieldStyles,
   InputLabel,
+  LoadingIndicator,
 } from '@components/shared/form';
 import { ApplicationStatusT } from '@hooks/applications/useGetApplicationStatuses.tsx';
 
@@ -24,7 +25,15 @@ const SelectApplicationStatus = <T extends FieldValues>({
   fieldId,
   defaultValue,
 }: ComponentPropsT<T>) => {
-  const { data } = useGetApplicationStatuses();
+  const { data, isLoading, isError } = useGetApplicationStatuses();
+
+  if (isLoading) {
+    return <LoadingIndicator content={'Loading form options.'} />;
+  }
+
+  if (isError) {
+    return <ErrorMessage content={'An error has occurred. Refresh the page or reach out via \'Feedback\'.'} />;
+  }
 
   return (
     <InputFieldStyles $isError={fieldError !== undefined}>
