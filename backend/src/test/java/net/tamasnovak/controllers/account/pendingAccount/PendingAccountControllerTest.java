71fd3e9abd59a6cb5d2e6f51b7122d1a9979cc61
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-04-29 20:56:26 +0200

    test: add happy case integration test to register() method in PendingAccountController class

---
+++ backend/src/test/java/net/tamasnovak/controllers/account/pendingAccount/PendingAccountControllerTest.java
@@ -0,0 +1,59 @@
+package net.tamasnovak.controllers.account.pendingAccount;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.tamasnovak.dtos.account.request.PendingAccountRegistrationDto;
+import net.tamasnovak.repositories.account.PendingAccountRepository;
+import net.tamasnovak.services.account.pendingAccount.PendingAccountService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Description;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.UUID;
+
+@WebMvcTest(controllers = PendingAccountController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class PendingAccountControllerTest {
+  @Autowired
+  MockMvc mockMvc;
+  @Autowired
+  ObjectMapper objectMapper;
+  @MockBean
+  PendingAccountService pendingAccountService;
+  @MockBean
+  PendingAccountRepository pendingAccountRepository;
+
+  @AfterEach
+  void tearDown() {
+    pendingAccountRepository.deleteAll();
+  }
+
+  @Nested
+  @DisplayName("register() method integration tests")
+  class RegisterMethodIntegrationTests {
+    @Test
+    @Description("HttpStatus.CREATED status is correctly asserted if no exceptions were thrown.")
+    void shouldReturnHttpStatusCreated_IfNoExceptionsWereThrown() throws Exception {
+      PendingAccountRegistrationDto requestBody = new PendingAccountRegistrationDto(
+        "Student",
+        "Test User",
+        "student@test.net",
+        UUID.randomUUID().toString()
+      );
+
+      mockMvc.perform(MockMvcRequestBuilders.post("/api/pending-accounts/register")
+        .content(objectMapper.writeValueAsString(requestBody))
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+  }
+}
