b919b106f28d95181583e9ef156914ba2fc8653c
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-05-06 20:30:45 +0200

    refactor: remove not needed methods in ValidatorUtilities

--- backend/src/main/java/net/tamasnovak/utilities/ValidatorUtilities.java
+++ backend/src/main/java/net/tamasnovak/utilities/ValidatorUtilities.java
@@ -2,29 +2,14 @@ package net.tamasnovak.utilities;
 
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
 import java.util.UUID;
 
 @Component
 public final class ValidatorUtilities {
   private ValidatorUtilities() {}
 
-  public UUID validateIfStringIsUuid(String uuid) {
-    try {
-      return UUID.fromString(uuid);
-    } catch (IllegalArgumentException | NullPointerException exception) {
-      throw new IllegalArgumentException("The request contained invalid data. Resubmit the form.");
-    }
-  }
-
   public void checkIfUuidsAreEqual(UUID uuid, UUID uuidToCheckAgainst, String exceptionMessage) {
     if (!uuid.equals(uuidToCheckAgainst)) {
-      throw new IllegalArgumentException(exceptionMessage);
-    }
-  }
-
-  public void checkIfApplicationMentorIsValid(long applicationMentorId, long authMentorId, String exceptionMessage) {
-    if (!Objects.equals(applicationMentorId, authMentorId)) {
       throw new IllegalArgumentException(exceptionMessage);
     }
   }
