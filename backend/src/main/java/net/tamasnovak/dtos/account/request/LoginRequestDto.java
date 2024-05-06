a8ed0f4554dcf36ec7f6cf962e852f1a33464846
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-05-06 20:31:51 +0200

    refactor: add empty line for better clarity

--- backend/src/main/java/net/tamasnovak/dtos/account/request/LoginRequestDto.java
+++ backend/src/main/java/net/tamasnovak/dtos/account/request/LoginRequestDto.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 public record LoginRequestDto(
   @Email(message = "Provide a valid email address.")
   String email,
+
   @NotBlank(message = "Provide your password.")
   String password
 ) {}
