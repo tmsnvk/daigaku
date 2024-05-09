4320b36d28a1497e2c652baf824c4f5ea1a99429
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-05-09 15:21:06 +0200

    style: delete blank character, deactivate eslint commenting rule

--- backend/src/main/java/net/tamasnovak/annotations/uuidValidation/UuidConstraint.java
+++ backend/src/main/java/net/tamasnovak/annotations/uuidValidation/UuidConstraint.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 
 @Documented
 @Constraint(validatedBy = UuidConstraintValidator.class)
-@Target( { ElementType.FIELD, ElementType.PARAMETER })
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UuidConstraint {
   String message() default "Record with this id was not found.";
