95c29193af440bec18b0a8f0e6bd0ccd37a3bd58
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-05-12 11:37:06 +0200

    fix: add missing annotation to ApplicationController.java

--- backend/src/main/java/net/tamasnovak/controllers/application/ApplicationController.java
+++ backend/src/main/java/net/tamasnovak/controllers/application/ApplicationController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/applications")
+@Validated
 public class ApplicationController {
   private final ApplicationService applicationService;
 
