589cef7318f5590a956b49cfc1c03a274fd6719a
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-05-21 14:11:55 +0200

    test: add unit tests for StudentServiceImpl.java

---
+++ backend/src/test/java/net/tamasnovak/services/account/accountByRole/student/StudentServiceImplTest.java
@@ -0,0 +1,65 @@
+package net.tamasnovak.services.account.accountByRole.student;
+
+import jakarta.persistence.EntityNotFoundException;
+import net.tamasnovak.entities.account.accountByRole.Mentor;
+import net.tamasnovak.entities.account.accountByRole.Student;
+import net.tamasnovak.entities.account.baseAccount.Account;
+import net.tamasnovak.repositories.account.accountByRole.StudentRepository;
+import net.tamasnovak.services.GlobalServiceConstants;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.annotation.Description;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceImplTest {
+  @Mock
+  private StudentRepository studentRepository;
+  @Mock
+  private GlobalServiceConstants globalServiceConstants;
+  @InjectMocks
+  private StudentServiceImpl underTest;
+
+  private final Account mockAccount = mock(Account.class);
+
+  @Nested
+  @DisplayName("getStudentByAccount() unit tests")
+  class GetStudentByAccountUnitTests {
+    @Test
+    @Description("Should return correct Student instance if Account was correctly received.")
+    public void shouldReturnStudentByAccount() {
+      Mentor mockMentor = mock(Mentor.class);
+
+      Student expected = Student.createStudent(mockAccount, mockMentor);
+      when(studentRepository.findByAccount(mockAccount)).thenReturn(Optional.of(expected));
+
+      Student actual = underTest.getStudentByAccount(mockAccount);
+      verify(studentRepository, times(1)).findByAccount(mockAccount);
+
+      assertEquals(expected, actual);
+    }
+
+    @Test
+    @Description("")
+    public void shouldThrowEntityNotFoundException_IfStudentIsNotFound() {
+      when(studentRepository.findByAccount(mockAccount)).thenReturn(Optional.empty());
+
+      assertThrows(EntityNotFoundException.class, () -> underTest.getStudentByAccount(mockAccount));
+
+      verify(studentRepository, times(1)).findByAccount(mockAccount);
+    }
+  }
+}
