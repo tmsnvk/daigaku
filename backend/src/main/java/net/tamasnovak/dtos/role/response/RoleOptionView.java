d4d052331f5430356f8aa72246e0c5a7b71f1a50
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-05-28 18:25:46 +0200

    feat: write getStudentAndMentorRoleOptions custom query

---
+++ backend/src/main/java/net/tamasnovak/dtos/role/response/RoleOptionView.java
@@ -0,0 +1,8 @@
+package net.tamasnovak.dtos.role.response;
+
+import java.util.UUID;
+
+public interface RoleOptionView {
+  UUID getUuid();
+  String getName();
+}
--- backend/src/main/java/net/tamasnovak/repositories/role/RoleRepository.java
+++ backend/src/main/java/net/tamasnovak/repositories/role/RoleRepository.java
@@ -1,10 +1,26 @@
 package net.tamasnovak.repositories.role;
 
+import net.tamasnovak.dtos.role.response.RoleOptionView;
 import net.tamasnovak.entities.role.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
   Optional<Role> findByNameContains(String name);
+
+  @Query(value =
+    """
+      SELECT
+        uuid,
+        name
+      FROM
+        roles
+      WHERE
+        name = 'ROLE_STUDENT' OR
+        name = 'ROLE_MENTOR'
+    """, nativeQuery = true)
+  List<RoleOptionView> getStudentAndMentorRoleOptions();
 }
