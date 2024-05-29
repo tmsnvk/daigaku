9df2781a7a2f52ef6cc4d34885044ed53e2a6b2c
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-05-29 17:01:26 +0200

    feat: update pom.xml and contextLoads test case

--- backend/src/test/java/net/tamasnovak/backend/DaigakuBackendApplicationTests.java
+++ backend/src/test/java/net/tamasnovak/backend/DaigakuBackendApplicationTests.java
@@ -2,9 +2,14 @@ package net.tamasnovak.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class DaigakuBackendApplicationTests {
   @Test
-  void contextLoads() {}
+  void contextLoads(ApplicationContext context) {
+    assertThat(context).isNotNull();
+  }
 }
