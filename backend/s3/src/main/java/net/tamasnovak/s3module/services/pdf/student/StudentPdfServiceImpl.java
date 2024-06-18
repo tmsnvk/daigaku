f308854e06f12a5bef3e9f3965241ff4303a004a
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-06-18 20:30:40 +0200

    feat: update createStudentApplicationsPdf method with deleting the temporary pdf from spring backend folder

--- backend/s3/src/main/java/net/tamasnovak/s3module/services/pdf/student/StudentPdfServiceImpl.java
+++ backend/s3/src/main/java/net/tamasnovak/s3module/services/pdf/student/StudentPdfServiceImpl.java
@@ -1,13 +1,11 @@
-package net.tamasnovak.s3module.services.pdf.student;
+package net.tamasnovak.services.documentGenerator.pdf;
 
 import com.itextpdf.html2pdf.HtmlConverter;
-import net.tamasnovak.rabbitmq.configuration.rabbitmq.RabbitMQCommonConfig;
-import net.tamasnovak.rabbitmq.models.application.StudentApplicationQueueDto;
-import net.tamasnovak.rabbitmq.models.queueDto.StudentPdfSaveQueueDto;
-import net.tamasnovak.s3module.services.amazonS3Service.AmazonS3Service;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import net.tamasnovak.domains.account.account.models.entity.Account;
+import net.tamasnovak.domains.application.shared.models.dtoResponses.ApplicationDto;
+import net.tamasnovak.domains.support.institution.models.entity.Institution;
+import net.tamasnovak.services.amazonS3Service.AmazonS3Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,18 +13,17 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Service
-public class StudentPdfServiceImpl implements StudentPdfService {
-  @Value("${aws.endpointUrl}")
-  private String endpointUrlRoot;
-
+public class PdfServiceImpl implements PdfService {
   private final AmazonS3Service amazonS3Service;
   private final PdfServiceConstants pdfServiceConstants;
   private final StudentApplicationsConstants studentApplicationsConstants;
 
   @Autowired
-  public StudentPdfServiceImpl(AmazonS3Service amazonS3Service, PdfServiceConstants pdfServiceConstants, StudentApplicationsConstants studentApplicationsConstants) {
+  public PdfServiceImpl(AmazonS3Service amazonS3Service, PdfServiceConstants pdfServiceConstants, StudentApplicationsConstants studentApplicationsConstants) {
     this.amazonS3Service = amazonS3Service;
     this.pdfServiceConstants = pdfServiceConstants;
     this.studentApplicationsConstants = studentApplicationsConstants;
@@ -34,54 +31,51 @@ public class StudentPdfServiceImpl implements StudentPdfService {
 
   @Override
   @Transactional
-  @RabbitListener(queues = { RabbitMQCommonConfig.STUDENT_PDF_SAVE_QUEUE_KEY })
-  public void createStudentApplicationsPdf(StudentPdfSaveQueueDto messageQueueDto) {
+  public void createStudentApplicationsPdf(Account studentAccount, Institution studentInstitution, UUID authAccountUuid, List<ApplicationDto> applications) {
     try {
-      StringBuilder studentData = compileStudentData(messageQueueDto);
-      StringBuilder applicationData = compileStudentApplicationsDynamicData(messageQueueDto);
+      StringBuilder studentData = compileStudentData(studentAccount, studentInstitution);
+      StringBuilder applicationData = compileStudentApplicationsDynamicData(applications);
 
       LocalDateTime now = LocalDateTime.now();
 
-      String htmlSource = String.format(studentApplicationsConstants.STUDENT_APPLICATION_HTML_MAIN_TEMPLATE,
+      String htmlSource = String.format(studentApplicationsConstants.StudentApplicationsCentralTemplate,
         now.toLocalDate(),
         now.toLocalTime(),
         studentData,
         applicationData
       );
 
-      File file = new File(String.format("%s.pdf", messageQueueDto.authAccountUuid()));
+      File file = new File(String.format("%s.pdf", authAccountUuid));
       HtmlConverter.convertToPdf(htmlSource, new FileOutputStream(file));
 
       amazonS3Service.uploadFileToS3Bucket(file.toString(), file);
 
       file.delete();
-
-//      return endpointUrlRoot + String.format("%s.pdf", authAccountUuid);
     } catch (IOException exception) {
       throw new IllegalStateException(pdfServiceConstants.PDF_ERROR);
     }
   }
 
-  private StringBuilder compileStudentData(StudentPdfSaveQueueDto messageQueueDto) {
+  private StringBuilder compileStudentData(Account studentAccount, Institution studentInstitution) {
     StringBuilder studentData = new StringBuilder();
 
     studentData.append(
-      String.format(studentApplicationsConstants.STUDENT_HTML_STUDENT_DATA,
-        messageQueueDto.studentAccount().fullName(),
-        messageQueueDto.studentAccount().email(),
-        messageQueueDto.studentAccount().institutionName()
+      String.format(studentApplicationsConstants.StudentDataChunk,
+        studentAccount.getFullName(),
+        studentAccount.getEmail(),
+        studentInstitution.getName()
       )
     );
 
     return studentData;
   }
 
-  private StringBuilder compileStudentApplicationsDynamicData(StudentPdfSaveQueueDto messageQueueDto) {
+  private StringBuilder compileStudentApplicationsDynamicData(List<ApplicationDto> applications) {
     StringBuilder applicationData = new StringBuilder();
 
-    for (StudentApplicationQueueDto application : messageQueueDto.applications()) {
+    for (ApplicationDto application : applications) {
       applicationData.append(
-        String.format(studentApplicationsConstants.STUDENT_HTML_APPLICATIONS_DATA,
+        String.format(studentApplicationsConstants.SingleApplicationDataChunk,
           application.createdAt(),
           application.lastUpdatedAt(),
           application.courseName(),
