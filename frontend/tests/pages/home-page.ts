566eec2eaaf241295369e1ab4f1850d2fac3f9a9
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-07-02 12:33:24 +0200

    test: update playwright config file

--- frontend/tests/pages/home-page.ts
+++ frontend/tests/pages/home-page.ts
@@ -37,7 +37,7 @@ export class HomePage {
   }
 
   goToNoAuthHomePage = async () => {
-    await this.page.goto('./');
+    await this.page.goto('http://127.0.0.1:5173/');
   };
 
   verifyFormSectionElement = async () => {
