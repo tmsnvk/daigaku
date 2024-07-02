2168f650d8c63ebabc87d1794f7a8ba6e10b7e18
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-07-02 12:25:08 +0200

    test: update playwright config file

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -13,10 +13,10 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     reuseExistingServer: !process.env.CI,
-    url: 'http://localhost:5173/',
+    url: 'http://127.0.0.1:5173/',
   },
   use: {
-    baseURL: 'http://localhost:5173/',
+    baseURL: 'http://127.0.0.1:5173/',
     trace: 'on-first-retry',
   },
   projects: [
