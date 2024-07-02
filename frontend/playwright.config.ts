ca92e60f9bfe7e142303beee8aed0efe1b9a0314
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-07-02 10:23:41 +0200

    test: update playwright config file

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     url: 'http://127.0.0.1:5173/',
   },
   use: {
-    baseURL: 'http://127.0.0.1:5173/',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://127.0.0.1:5173/',
     trace: 'on-first-retry',
   },
   projects: [
