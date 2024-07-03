73781b99aca1a0ec9d4877472a7330156d204465
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-07-03 16:37:43 +0200

    style: adjust base stylings

--- frontend/src/components/base-styles/BaseDarkBorder/BaseDarkBorder.styles.ts
+++ frontend/src/components/base-styles/BaseDarkBorder/BaseDarkBorder.styles.ts
@@ -2,8 +2,8 @@ import { css } from 'styled-components';
 
 const BaseDarkBorder = css`
   background-color: ${({ theme }) => theme.color.secondaryLight};
-  border: 0.25rem solid ${({ theme }) => theme.color.secondaryLight};
-  border-radius: 0.75rem;
+  border: 0.1rem solid ${({ theme }) => theme.color.secondaryLight};
+  border-radius: ${({ theme }) => theme.options.borderRadius};
   box-shadow: 0 0.1rem 1.5rem 0 ${({ theme }) => theme.color.primaryDark};
 `;
 
--- frontend/src/components/base-styles/BaseLightBorder/BaseLightBorder.styles.ts
+++ frontend/src/components/base-styles/BaseLightBorder/BaseLightBorder.styles.ts
@@ -3,7 +3,7 @@ import { css } from 'styled-components';
 const BaseLightBorder = css`
   background-color: ${({ theme }) => theme.color.primaryLight};
   border: 0.1rem solid ${({ theme }) => theme.color.secondaryLight};
-  border-radius: 1.5rem;
+  border-radius: ${({ theme }) => theme.options.borderRadius};
   box-shadow: 0.5rem 0.5rem 1rem ${({ theme }) => theme.color.primaryDark};
 `;
 
