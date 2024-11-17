c56b373f7ea391f3524c22ba5c50c06b1669d75c
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-11-17 15:29:51 +0100

    88 - fix and extend account-related unit tests

--- backend/core/src/test/java/net/tamasnovak/artifact/account/pendingaccount/dto/PendingAccountRegistrationRequestTest.java
+++ backend/core/src/test/java/net/tamasnovak/artifact/account/pendingaccount/dto/PendingAccountRegistrationRequestTest.java
@@ -1,8 +1,17 @@
+/**
+ * Copyright © [Daigaku].
+ * This file contains proprietary code.
+ * Unauthorized copying, modification, or distribution of this file, whether in whole or in part is prohibited.
+ *
+ * @author tmsnvk
+ */
+
 package net.tamasnovak.artifact.account.pendingaccount.dto;
 
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
@@ -12,11 +21,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Description;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PendingAccountRegistrationRequestTest {
-  private final String expectedValidEmail = "notexistingemail@test.net";
+  private final String expectedValidEmail = "expectedvalidemail@test.net";
   private final String validUuidString = UUID.randomUUID().toString();
   Set<ConstraintViolation<PendingAccountRegistrationRequest>> violations = new HashSet<>();
   PendingAccountRegistrationRequest underTest = null;
@@ -28,18 +39,12 @@ class PendingAccountRegistrationRequestTest {
   }
 
   @Nested
-  @DisplayName("PendingAccountRegistration validation no-violation unit tests")
+  @DisplayName("PendingAccountRegistrationRequest validation no-violation unit tests")
   class WhenDtoIsValid {
     @Test
-    @Description("Assert that there are no violations when underTest requestBody contains only valid fields.")
+    @Description("Asserts that there are no violations when underTest contains only valid properties.")
     void shouldAssert_thatViolationsSetIsEmpty_whenRequestBodyIsValid() {
-      underTest = new PendingAccountRegistrationRequest(
-        "Valid",
-        "User",
-        expectedValidEmail,
-        validUuidString,
-        validUuidString
-      );
+      underTest = new PendingAccountRegistrationRequest("Valid", "User", expectedValidEmail, validUuidString, validUuidString);
 
       violations = validate(underTest);
 
@@ -51,156 +56,161 @@ class PendingAccountRegistrationRequestTest {
   @DisplayName("PendingAccountRegistration validation violation unit tests")
   class WhenDtoIsInvalid {
     @Test
-    @Description("Assert that there is a violation when underTest requestBody's firstName fails @NotBlank validation.")
+    @Description("Asserts that there is a violation when underTest's firstName fails @NotBlank validation.")
     void shouldAssert_thatViolationsSetIsNotEmpty_whenFirstNameFailsNotBlankValidation() {
-      underTest = new PendingAccountRegistrationRequest(
-        "",
-        "User",
-        expectedValidEmail,
-        validUuidString,
-        validUuidString
-      );
+      underTest = new PendingAccountRegistrationRequest("", "User", expectedValidEmail, validUuidString, validUuidString);
 
       violations = validate(underTest);
-      String actualViolationType = violations.iterator().next().getPropertyPath().toString();
+      String actualViolationPath = violations.iterator().next().getPropertyPath().toString();
 
-      assertEquals("firstName", actualViolationType);
+      assertEquals("firstName", actualViolationPath);
     }
 
     @Test
-    @Description("Assert that there is a violation when underTest requestBody's firstName fails @Pattern validation.")
+    @Description("Asserts that there is a violation when underTest's firstName fails @Pattern validation.")
     void shouldAssert_thatViolationsSetIsNotEmpty_whenFirstNameFailsPatternValidation() {
-      underTest = new PendingAccountRegistrationRequest(
-        "Inv4l-d",
-        "User",
-        expectedValidEmail,
-        validUuidString,
-        validUuidString
-      );
+      underTest = new PendingAccountRegistrationRequest("Inv4l-d", "User", expectedValidEmail, validUuidString, validUuidString);
 
       violations = validate(underTest);
-      String actualViolationType = violations.iterator().next().getPropertyPath().toString();
+      String actualViolationPath = violations.iterator().next().getPropertyPath().toString();
 
-      assertEquals("firstName", actualViolationType);
+      assertEquals("firstName", actualViolationPath);
     }
 
     @Test
-    @Description("Assert that there is a violation when underTest requestBody's lastName fails @NotBlank validation.")
+    @Description("Asserts that there is a violation when underTest's lastName fails @NotBlank validation.")
     void shouldAssert_thatViolationsSetIsNotEmpty_whenLastNameFailsNotBlankValidation() {
-      underTest = new PendingAccountRegistrationRequest(
-        "Valid",
-        "",
-        expectedValidEmail,
-        validUuidString,
-        validUuidString
-      );
+      underTest = new PendingAccountRegistrationRequest("Valid", "", expectedValidEmail, validUuidString, validUuidString);
 
       violations = validate(underTest);
-      String actualViolationType = violations.iterator().next().getPropertyPath().toString();
+      String actualViolationPath = violations.iterator().next().getPropertyPath().toString();
 
-      assertEquals("lastName", actualViolationType);
+      assertEquals("lastName", actualViolationPath);
     }
 
     @Test
-    @Description("Assert that there is a violation when underTest requestBody's lastName fails @Pattern validation.")
+    @Description("Asserts that there is a violation when underTest's lastName fails @Pattern validation.")
     void shouldAssert_thatViolationsSetIsNotEmpty_whenLastNameFailsPatternValidation() {
-      underTest = new PendingAccountRegistrationRequest(
-        "Valid",
-        "!s3r",
-        expectedValidEmail,
-        validUuidString,
-        validUuidString
-      );
+      underTest = new PendingAccountRegistrationRequest("Valid", "!s3r", expectedValidEmail, validUuidString, validUuidString);
 
       violations = validate(underTest);
-      String actualViolationType = violations.iterator().next().getPropertyPath().toString();
+      String actualViolationPath = violations.iterator().next().getPropertyPath().toString();
 
-      assertEquals("lastName", actualViolationType);
+      assertEquals("lastName", actualViolationPath);
     }
 
     @Test
-    @Description("Assert that there is a violation when underTest requestBody's email fails @Email validation.")
+    @Description("Asserts that there is a violation when underTest's email fails @Email validation.")
     void shouldAssert_thatViolationsSetIsNotEmpty_whenEmailFailsEmailValidation() {
-      underTest = new PendingAccountRegistrationRequest(
-        "Valid",
-        "User",
-        "invalidemail.com",
-        validUuidString,
-        validUuidString
-      );
+      underTest = new PendingAccountRegistrationRequest("Valid", "User", "invalidemail.com", validUuidString, validUuidString);
 
       violations = validate(underTest);
-      String actualViolationType = violations.iterator().next().getPropertyPath().toString();
+      String actualViolationPath = violations.iterator().next().getPropertyPath().toString();
 
-      assertEquals("email", actualViolationType);
+      assertEquals("email", actualViolationPath);
     }
 
     @Test
-    @Description("Assert that there is a violation when underTest requestBody's institutionUuid fails @NotBlank validation.")
+    @Description("Asserts that there is a violation when underTest's institutionUuid fails @NotBlank validation.")
     void shouldAssert_thatViolationsSetIsNotEmpty_whenInstitutionUuidFailsNotBlankValidation() {
-      underTest = new PendingAccountRegistrationRequest(
-        "Valid",
-        "User",
-        expectedValidEmail,
-        "",
-        validUuidString
-      );
+      underTest = new PendingAccountRegistrationRequest("Valid", "User", expectedValidEmail, "", validUuidString);
 
       violations = validate(underTest);
-      String actualViolationType = violations.iterator().next().getPropertyPath().toString();
+      String actualViolationPath = violations.iterator().next().getPropertyPath().toString();
 
-      assertEquals("institutionUuid", actualViolationType);
+      assertEquals("institutionUuid", actualViolationPath);
     }
 
     @Test
-    @Description("Assert that there is a violation when underTest requestBody's institutionUuid fails @UuidConstraint validation.")
+    @Description("Asserts that there is a violation when underTest's institutionUuid fails @UuidConstraint validation.")
     void shouldAssert_thatViolationsSetIsNotEmpty_whenInstitutionUuidFailsUuidConstraintValidation() {
-      underTest = new PendingAccountRegistrationRequest(
-        "Valid",
-        "User",
-        expectedValidEmail,
-        "validUuid",
-        validUuidString
-      );
+      underTest = new PendingAccountRegistrationRequest("Valid", "User", expectedValidEmail, "invalidUuid", validUuidString);
 
       violations = validate(underTest);
-      String actualViolationType = violations.iterator().next().getPropertyPath().toString();
+      String actualViolationPath = violations.iterator().next().getPropertyPath().toString();
 
-      assertEquals("institutionUuid", actualViolationType);
+      assertEquals("institutionUuid", actualViolationPath);
     }
 
     @Test
-    @Description("Assert that there is a violation when underTest requestBody's accountRoleUuid fails @NotBlank validation.")
+    @Description("Asserts that there is a violation when underTest's accountRoleUuid fails @NotBlank validation.")
     void shouldAssert_thatViolationsSetIsNotEmpty_whenAccountTypeFailsNotBlankValidation() {
-      underTest = new PendingAccountRegistrationRequest(
-        "Valid",
-        "User",
-        expectedValidEmail,
-        validUuidString,
-        ""
-      );
+      underTest = new PendingAccountRegistrationRequest("Valid", "User", expectedValidEmail, validUuidString, "");
 
       violations = validate(underTest);
-      String actualViolationType = violations.iterator().next().getPropertyPath().toString();
+      String actualViolationPath = violations.iterator().next().getPropertyPath().toString();
 
-      assertEquals("accountRoleUuid", actualViolationType);
+      assertEquals("accountRoleUuid", actualViolationPath);
     }
 
     @Test
-    @Description("Assert that there is a violation when underTest requestBody's accountRoleUuid fails @UuidConstraint validation.")
+    @Description("Asserts that there is a violation when underTest's accountRoleUuid fails @UuidConstraint validation.")
     void shouldAssert_thatViolationsSetIsNotEmpty_whenAccountTypeFailsPatternValidation() {
-      underTest = new PendingAccountRegistrationRequest(
-        "Valid",
-        "User",
-        expectedValidEmail,
-        validUuidString,
-        "validUuid"
-      );
+      underTest = new PendingAccountRegistrationRequest("Valid", "User", expectedValidEmail, validUuidString, "invalidUuid");
 
       violations = validate(underTest);
-      String actualViolationType = violations.iterator().next().getPropertyPath().toString();
+      String actualViolationPath = violations.iterator().next().getPropertyPath().toString();
 
-      assertEquals("accountRoleUuid", actualViolationType);
+      assertEquals("accountRoleUuid", actualViolationPath);
+    }
+
+    @Test
+    @Description("Asserts that there are multiple violations when underTest's lastName fails @Pattern, email fails @Email and "
+      + "accountRoleUuid fails @UuidConstraint validation.")
+    void shouldAssert_thatViolationSetIsNotEmpty_whenRequestBodyFailsMultipleValidations() {
+      underTest = new PendingAccountRegistrationRequest("Valid", "!s3r", "invalidemail.com", validUuidString, "invalidUuid");
+      Set<String> expectedViolationPaths = Set.of("lastName", "email", "accountRoleUuid");
+
+      violations = validate(underTest);
+      Set<String> actualViolationPaths = violations.stream()
+                                                   .map(violation -> violation.getPropertyPath().toString())
+                                                   .collect(Collectors.toSet());
+
+      assertEquals(expectedViolationPaths, actualViolationPaths);
+    }
+  }
+
+  @Nested
+  @DisplayName("Institution uuid getter unit tests")
+  class InstitutionUuidGetterUnitTests {
+    @Test
+    @Description("Asserts that getInstituionUuid() uuid transformation is valid when the provided uuid is valid.")
+    void shouldAssert_thatInstitutionUuidTransformationIsValid_withValidUuid() {
+      underTest = new PendingAccountRegistrationRequest("Valid", "User", expectedValidEmail, validUuidString, validUuidString);
+
+      UUID actualInstitutionUuid = underTest.getInstituionUuid();
+
+      assertEquals(UUID.fromString(validUuidString), actualInstitutionUuid);
+    }
+
+    @Test
+    @Description("Asserts that getInstituionUuid() uuid transformation fails when the provided uuid is invalid.")
+    void shouldAssert_thatInstitutionUuidTransformationFails_withInvalidUuid() {
+      underTest = new PendingAccountRegistrationRequest("Valid", "User", expectedValidEmail, "invalidUuid", validUuidString);
+
+      assertThrows(IllegalArgumentException.class, () -> underTest.getInstituionUuid());
+    }
+  }
+
+  @Nested
+  @DisplayName("Role uuid getter unit tests")
+  class RoleUuidGetterUnitTests {
+    @Test
+    @Description("Asserts that getAccountRoleUuid() uuid transformation is valid when the provided uuid is valid.")
+    void shouldAssert_thatAccountRoleUuidTransformationIsValid_withValidUuid() {
+      underTest = new PendingAccountRegistrationRequest("Valid", "User", expectedValidEmail, validUuidString, validUuidString);
+
+      UUID actualAccountRoleUuid = underTest.getAccountRoleUuid();
+
+      assertEquals(UUID.fromString(validUuidString), actualAccountRoleUuid);
+    }
+
+    @Test
+    @Description("Asserts that getAccountRoleUuid() uuid transformation fails when the provided uuid is invalid.")
+    void shouldAssert_thatAccountRoleUuidTransformationFails_withInvalidUuid() {
+      underTest = new PendingAccountRegistrationRequest("Valid", "User", expectedValidEmail, validUuidString, "invalidUuid");
+
+      assertThrows(IllegalArgumentException.class, () -> underTest.getAccountRoleUuid());
     }
   }
 
