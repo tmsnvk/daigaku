5905c3de7404063cd468f17ec78b0d65b7720a68
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-12-08 13:57:48 +0100

    88 - fix and extend account-related unit tests

--- frontend/tests/pages/new-application-page/new-application-page.parameters.ts
+++ frontend/tests/pages/new-application-page/new-application-page.parameters.ts
@@ -3,14 +3,12 @@
  */
 
 /**
- * @fileoverview
- * @author tmsnvk
- *
- *
  * Copyright © [Daigaku].
  *
  * This file contains proprietary code.
  * Unauthorized copying, modification, or distribution of this file, whether in whole or in part is prohibited.
+ *
+ * @author tmsnvk
  */
 
 export const newApplicationParameters = [
