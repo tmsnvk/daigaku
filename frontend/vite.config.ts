cc534cabc5cea63978d91efe40beb4baa77a0caf
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-12-15 08:55:45 +0100

    amend github action workflow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,6 @@
 
 /* external imports */
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -36,6 +35,8 @@ export default defineConfig({
     },
   },
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+    alias: {
+      src: '/src',
+    },
   },
 });
