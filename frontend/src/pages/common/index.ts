73962082d036669b3a4145844acbc184a49109bf
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-12-15 10:10:12 +0100

    amend github action workflow

--- frontend/src/pages/common/index.ts
+++ frontend/src/pages/common/index.ts
@@ -11,9 +11,9 @@
  * @author tmsnvk
  */
 
+import { Applications } from '../../../src/pages/common/applications/index';
 import { ApplicationEdit } from './application-edit';
 import { ApplicationView } from './application-view';
-import { Applications } from './applications';
 import { Dashboard } from './dashboard';
 import { Error } from './error';
 import { Home } from './home';
