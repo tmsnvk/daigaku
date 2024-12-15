3959d442af8ac5cfd57f2954f248f7090c4b99a5
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-12-15 08:41:37 +0100

    amend github action workflow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -36,9 +36,6 @@ export default defineConfig({
     },
   },
   resolve: {
-    alias: {
-      'src': '/src',
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
   },
 });
