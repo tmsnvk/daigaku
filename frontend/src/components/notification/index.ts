cc70ddcea380299ee9f543eeca9b069b82695477
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2024-12-15 11:06:03 +0100

    amend github action workflow

--- frontend/src/components/notification/index.ts
+++ frontend/src/components/notification/index.ts
@@ -14,6 +14,6 @@
 import { ConfirmationModal } from './confirmation-modal';
 import { GlobalErrorModal } from './global-error-modal';
 import { GlobalLoadingModal } from './global-loading-modal';
-import { Toast } from './toasts';
+import { Toast } from './toast';
 
 export { ConfirmationModal, GlobalErrorModal, GlobalLoadingModal, Toast };
