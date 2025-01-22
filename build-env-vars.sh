d6aa33d866652b5bbdbf478d565211cc6c9c2400
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2025-01-22 14:37:34 +0100

    123-update-readmemd (#124)

--- build-env-vars.sh
+++ build-env-vars.sh
@@ -4,7 +4,7 @@ cp .env_sample .env
 cp .env-test_sample .env-test
 
 echo "The script execution completed successfully. \
-.env files were created in the root directory. \
+Two .env files were created in the root directory. \
 Fill in the files with the necessary configuration details and environment variables, then run 'bash setup-containers.sh' from the root directory."
 
 exit 0
