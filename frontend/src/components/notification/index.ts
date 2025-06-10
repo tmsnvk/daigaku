1a0aea0f43f189811ea3be83cbfb261683c905bd
Tamas Novak <dev@tamasnovak.net>
2025-06-10 18:43:00 +0200

    fix: fix errors

--- frontend/src/components/notification/index.ts
+++ frontend/src/components/notification/index.ts
@@ -5,6 +5,6 @@
  */
 
 import { GlobalErrorModal } from './global-error-modal.tsx';
-import { Toast, type ToastVariantIntent, toastVariants } from './toast.tsx';
+import { Toast, type ToastVariantIntent } from './toast.tsx';
 
-export { GlobalErrorModal, Toast, toastVariants, type ToastVariantIntent };
+export { GlobalErrorModal, Toast, type ToastVariantIntent };
